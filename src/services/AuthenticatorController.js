a22b481e05196b28a42520e875c5b3c7f28de163
Marcos Murillo <marcos.felix@ies2.com.br>
2019-11-22 15:41:54 -0300

    segundo fator de autenticação

--- src/services/AuthenticatorController.js
+++ src/services/AuthenticatorController.js
@@ -3,14 +3,9 @@ import axios from 'axios';
 export default class Authenticator {
   static async validateMethod(pin) {
     let response = {};
-    try {
-      response = await axios.get(
-        `https://www.authenticatorApi.com/Validate.aspx?Pin=${pin}&SecretCode=ReactApp299`,
-        { crossDomain: true }
-      );
-    } catch (e) {
-      response = e;
-    }
+    response = await axios.get(
+      `https://cors-anywhere.herokuapp.com/https://www.authenticatorApi.com/Validate.aspx?Pin=${pin}&SecretCode=ReactApp299`
+    );
 
     return response;
   }
